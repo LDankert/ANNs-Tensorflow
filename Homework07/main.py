d91deaf62924821d4f3a36d8025e0d4012ad41bb
ldankert <ldankert@uni-osnabrueck.de>
2021-12-12 11:11:26 +0100

    working generator

--- Homework07/main.py
+++ Homework07/main.py
@@ -10,20 +10,24 @@ import numpy as np
 import tensorflow as tf
 from preprocessing import preprocess_dataset, integration_task
 
-sequence_length = 5     # will be imported from preprocessing module
-number_of_samples = 200 # will be imported from preprocessing module
+sequence_length = 2  # will be imported from preprocessing module
+number_of_samples = 200  # will be imported from preprocessing module
+
 
 # Iterates over integration_task with specific values
 def my_integration_task():
     yield next(integration_task(sequence_length, number_of_samples))
 
+
 # generating datasets output_signature adds metadata to the dataset like dtype
-ds = tf.data.Dataset.from_generator(my_integration_task, output_signature=tf.TensorSpec(shape=(5,1),dtype=tf.float32))
+ds = tf.data.Dataset.from_generator(my_integration_task, output_signature=(
+    tf.TensorSpec(shape=sequence_length, dtype=tf.float32),
+    tf.TensorSpec(shape=None, dtype=tf.float32)))
 
 # preprocessing the dataset
 ds = preprocess_dataset(ds)
 
-#print(next(my_integration_task())[0])
+# print(next(my_integration_task())[0])
 
 for element in ds:
     print(element)
